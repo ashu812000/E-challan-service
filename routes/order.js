b31ca3a81db1c5519f7b945aee3eaf6b7f869109
nitish-yadav-robomq <nitish.yadav@robomq.io>
2025-07-03 23:23:32 +0530

    updated routes

--- routes/order.js
+++ routes/order.js
@@ -6,11 +6,11 @@ const verifyAuth = require("../middleware/authmiddleware");
 const upload = multer({storage: multer.memoryStorage()});
 router.post("/", orderController.doOrder);
 router.get('/',verifyAuth, orderController.fetchOrderCounts)
-router.get("/allOrders",orderController.getAllOrders);
+router.get("/allOrders",verifyAuth,orderController.getAllOrders);
 router.post('/calculatePrice',orderController.calculatePrice);
 router.post('/update',verifyAuth,orderController.updateOrder);
-router.get("/:orderId", orderController.fetchOrder);
+router.get("/:orderId", verifyAuth,orderController.fetchOrder);
 router.post('/:orderId/payment-proof',upload.single("image"),orderController.paymentProof)
-router.get('/:orderId/payment-proof',orderController.getPaymentProof)
+router.get('/:orderId/payment-proof',verifyAuth,orderController.getPaymentProof)
 
 module.exports = router;
